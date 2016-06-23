2bba02e1593484278988c4283681ecf570b61377
Priyanka Nath <pnath.it@gmail.com>
2016-06-23 12:53:06 +0530

    svd of covariance

--- numpytest.py
+++ numpytest.py
@@ -7,14 +7,14 @@ fin.readline()
 l = [ map(float,line.split(' ')) for line in fin ]
 X = np.array(l)
 covariance =  np.cov(X.T)
-U,S,V = np.linalg.svd(X.T)
+U,S,V = np.linalg.svd(covariance)
 print "U=",U,'\n'
 print "S=",S,'\n'
 print "V=",V,'\n'
 
-q, r = np.linalg.qr(X)
-print "Q=",q,'\n'
-print "R=",r,'\n'
+#q, r = np.linalg.qr(X)
+#print "Q=",q,'\n'
+#print "R=",r,'\n'
 
 #np.savetxt('output_py.txt', covariance, fmt = '%.6f')
 
