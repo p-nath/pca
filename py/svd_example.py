cd0156cf994bbb0a268ec44f544d3106526759bf
Priyanka Nath <pnath.it@gmail.com>
2016-07-18 00:52:48 +0530

    fixed get_input_filename()

--- py/svd_example.py
+++ py/svd_example.py
@@ -1,14 +1,19 @@
 import numpy as np
 import sys
 
+def help():
+  print sys.argv[0], "-i <input_filename>"
+  exit(1)
 
-def get_finput():
+def get_input_filename():
   if (len(sys.argv) > 1):
-    return sys.argv[1]
+    if (sys.argv[1] == "-i") and (2 < len(sys.argv)):
+      return sys.argv[2]
+  help()
 
 def main():
-  finput = get_finput()
-  fin = open(finput, 'r')
+  input_filename = get_input_filename()
+  fin = open(input_filename, 'r')
   fin.readline()
   l = [ map(float,line.split(' ')) for line in fin ]
   X = np.array(l)
